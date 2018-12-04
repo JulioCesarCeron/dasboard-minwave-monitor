2d9a23d7b90367bf0c725872105e16416d2777d7
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-12-04 00:37:01 -0200

    identify values from eSense algorithm

--- dashboard-front/src/App.js
+++ dashboard-front/src/App.js
@@ -4,28 +4,48 @@ import { subscribeToTimer } from './api';
 
 class App extends Component {
 	state = {
-		data: 'no data yet'
+		poor_signal: 0,
+		attention: 0,
+		meditation: 0
 	};
 
-	constructor(props) {
-		super(props);
-
+	componentDidMount() {
 		subscribeToTimer((err, data) => {
-			this.setState({
-				data: data
-			});
+			this.identifyValues(data);
 		});
 	}
 
-	renderDataFromArduino() {
+	identifyValues = (data) => {
+		//P:0A:56M:83
 
+		if (data.indexOf('P:') !== -1) {
+			const poor = data.substring(0, data.indexOf('A')).replace('P:', '');
+			this.setState({
+				poor_signal: poor
+			});
+		}
+
+		if (data.indexOf('A:') !== -1) {
+			const attention = data.substring(data.indexOf('A:'), data.indexOf('M')).replace('A:', '');
+			this.setState({
+				attention: attention
+			});
+        }
         
+        if (data.indexOf('M:') !== -1) {
+			const meditation = data.substring(data.indexOf('M:'), data.length).replace('M:', '');
+			this.setState({
+				meditation: meditation
+			});
+		}
+	};
 
+	renderDataFromArduino() {
 		return (
 			<div className="App-h1-center">
-				<h1>SINAL: {this.state.data} </h1>
-				<h1>ATENÇÃO: {this.state.data} </h1>
-				<h1>MEDITAÇÃO: {this.state.data} </h1>
+				<h1>SINAL: {this.state.poor_signal} </h1>
+				<h1 style={ this.state.attention >= 60 ? {color: 'green'} : {color: 'white'}} >ATENÇÃO: {this.state.attention} </h1>
+				<h1>MEDITAÇÃO: {this.state.meditation} </h1>
 			</div>
 		);
 	}
